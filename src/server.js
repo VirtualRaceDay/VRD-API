fab81295a01b3d6257f4c955fd70ae10d6c47528
George MacKay-Shore <mangohatface@bigfatsodacompany.com>
2020-07-21 08:33:05 +0100

    Add CORS to server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import racedayRoutes from './routes/racedayRoutes';
 
 const server = express();
 const router = express.Router();
+
+server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
