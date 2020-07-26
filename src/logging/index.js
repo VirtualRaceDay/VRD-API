e90331d02e560a3d327e931bc55b7709a5134f07
George MacKay-Shore <mangohatface@bigfatsodacompany.com>
2020-07-27 00:27:38 +0100

    Add jest pacakges

--- src/logging/index.js
+++ src/logging/index.js
@@ -1,10 +1,12 @@
 import winston from 'winston';
 
+const silent = process.env.NODE_ENV === 'test';
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({ silent }),
   ]
 });
 
