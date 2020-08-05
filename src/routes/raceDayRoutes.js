3ae6780ae779e7b358cc0154068127d230242dbe
George MacKay-Shore <mangohatface@bigfatsodacompany.com>
2020-08-05 16:17:49 +0100

    Update route and controller

--- src/routes/raceDayRoutes.js
+++ src/routes/raceDayRoutes.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import * as RaceDayController from '../controllers/raceDayController';
 
-// Create a new router to handle the books resource
 const raceDayRoutes = express.Router();
 raceDayRoutes.route('/racedays')
   .get((req, res) => RaceDayController.getAllRaceDays(req, res))
   .post((req, res) => RaceDayController.createRaceDay(req, res));
+
+raceDayRoutes.route('/raceday/:id/leaderboard')
+  .get((req, res) => RaceDayController.getLeaderboardForRace(req, res));
+
 raceDayRoutes.route('/raceday/:id')
   .get((req, res) => RaceDayController.getRaceDayById(req, res))
   .put((req, res) => RaceDayController.updateRaceDay(req, res))
   .delete((req, res) => RaceDayController.deleteRaceDayById(req, res));
 
-// Export the router ready to be imported into an app.
 export default raceDayRoutes;
